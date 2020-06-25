2aa657245a9a36a18757796ce2719b4edf460695
Juan Rodriguez <jrodri89@uwo.ca>
2020-06-25 08:48:36 -0400

    Basic button linking and method setup.

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let quiz = [
+        Question(query: "4 + 2 = \(4 + 2)", answer: "True"),
+        Question(query: "5 = 3 > 1", answer: "True"),
+        Question(query: "8 + 3 < 10", answer: "False")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    func updateUI() {
+        
+    }
+    
 }
 
