00caa6500a12608ba5dda636b39c280b4c64ca09
Juan Rodriguez <jrodri89@uwo.ca>
2020-06-25 08:48:13 -0400

    Struct created for better handling of query-answer format.

---
+++ Quizzler/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Juan Rodriguez on 2020-06-25.
+//  Copyright © 2020 Juan Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let query: String
+    let answer: String
+}
